9522793e2490c3a5dfe0941db2f7ffef6b55189e
Rafael Akiyama <rafakiyama@gmail.com>
2026-02-07 23:39:44 -0300

    Projeto Concluído

--- cypress/pages/elements.js
+++ cypress/pages/elements.js
@@ -1,16 +1,16 @@
 export const ELEMENTS = {
-    
-  // Elementos do Login
+
+  // Elementos do Login //
   site: "https://www.saucedemo.com/",
   campoUsuarioLogin: "[data-test='username']",
   campoSenhaLogin: "[data-test='password']",
   botaoLogin: "[data-test='login-button']",
 
-  // Elementos da compra
+  // Elementos da compra //
   botaoAdicionarMochila: "[data-test='add-to-cart-sauce-labs-backpack']",
   botaoCarrinho: ".shopping_cart_link",
 
-  // Elementos Checkout
+  // Elementos Checkout //
   botaoCheckout: "[data-test='checkout']",
   nomeCheckout: "[data-test='firstName']",
   sobrenomeCheckout: "[data-test='lastName']",
